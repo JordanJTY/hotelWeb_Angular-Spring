6cdabb16cc8d8a63c712ac348083c8d942321805
SergioManuelJob <sergiomanuelsuarezsuarez@alumno.ieselrincon.es>
2023-02-16 19:19:11 +0000

    Apartments done in localDB

--- frontend/src/app/components/apartment-card/apartment-card.component.ts
+++ frontend/src/app/components/apartment-card/apartment-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
+import "aframe";
 
 @Component({
   selector: 'app-apartment-card',
--- frontend/src/app/views/home-page/home-page.component.ts
+++ frontend/src/app/views/home-page/home-page.component.ts
@@ -31,6 +31,18 @@ export class HomePageComponent {
 
   getAllApartment() {
     this.apartmentService.getAllApartments().subscribe(data => {
+
+      for(let i = 0 ; i <= data.length-1 ; i++){
+        this.db.table('myStore1').get(data[i].id!).then(data => {
+          if(data == undefined) console.log("Do nothing")
+          else{
+            this.db.table('myStore1').delete(data[i].id!).then(data => {
+              console.log("Deleted")
+            })
+          }
+        })
+      }
+
       data.forEach(element => {
         this.db.table('myStore1').add({
           id: element.id,
@@ -43,11 +55,14 @@ export class HomePageComponent {
           }
         )
       });
-      for(let i = 1 ; i <= data.length ; i++){
-        this.db.table('myStore1').get(i).then(data => {
+
+      for(let i = 0 ; i <= data.length-1 ; i++){
+        this.db.table('myStore1').get(data[i].id!).then(data => {
           this.apartment.push(new Apartment(data.type, data.img, data.typeImg, data.description, data.price, data.amount, data.id)) 
         })
       }
+
     })
   }
+
 }
